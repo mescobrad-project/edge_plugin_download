1ceabff8d406e450c3c307667b9fd144270a4cba
csaskaa <110460477+csaskaa@users.noreply.github.com>
2023-04-19 11:08:24 +0200

    Remove comparison of files between buckets (#4)

--- entrypoint.py
+++ entrypoint.py
@@ -4,19 +4,10 @@ from io import BytesIO
 class GenericPlugin(EmptyPlugin):
 
     def action(self, input_meta: PluginExchangeMetadata = None) -> PluginActionResponse:
-        import datetime
-        import os
         import boto3
         from botocore.client import Config
 
         # Init client
-        s3 = boto3.resource('s3',
-                            endpoint_url= self.__OBJ_STORAGE_URL__,
-                            aws_access_key_id= self.__OBJ_STORAGE_ACCESS_ID__,
-                            aws_secret_access_key= self.__OBJ_STORAGE_ACCESS_SECRET__,
-                            config=Config(signature_version='s3v4'),
-                            region_name=self.__OBJ_STORAGE_REGION__)
-
         s3_local = boto3.resource('s3',
                                   endpoint_url= self.__OBJ_STORAGE_URL_LOCAL__,
                                   aws_access_key_id= self.__OBJ_STORAGE_ACCESS_ID_LOCAL__,
@@ -24,15 +15,11 @@ class GenericPlugin(EmptyPlugin):
                                   config=Config(signature_version='s3v4'),
                                   region_name=self.__OBJ_STORAGE_REGION__)
 
-        # Compare buckets
+        # Download file from the local bucket to process
         bucket_local = s3_local.Bucket(self.__OBJ_STORAGE_BUCKET_LOCAL__)
-        bucket = s3.Bucket(self.__OBJ_STORAGE_BUCKET__)
-        obj_personal_data = bucket_local.objects.filter(Prefix="personal_data/", Delimiter="/")
-        obj_anonymous_data = bucket.objects.filter(Prefix="anonymous_data/", Delimiter="/")
+        obj_personal_data = bucket_local.objects.filter(Prefix="csv_data/", Delimiter="/")
 
-        keys_anonymous_data = [os.path.splitext(os.path.basename(obj.key))[0] for obj in obj_anonymous_data]
-        files_to_anonymize = [obj.key for obj in obj_personal_data \
-                              if os.path.splitext(os.path.basename(obj.key))[0] not in keys_anonymous_data]
+        files_to_anonymize = [obj.key for obj in obj_personal_data]
 
         # Download data
         output = []
